8abf1a6c9db607efd794bc61732856b4d806edd1
Munsif <alijunejo0007@gmail.com>
2024-03-24 02:10:50 +0500

    On branch day2  Your branch is up to date with 'origin/day2'.  Changes to be committed: 	modified:   lib/Pages/HomePage.dart

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -12,7 +12,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Material(
         child: Column(
-      children: [Text("Material!")],
+      children: [Text("hih!")],
     ));
   }
 }
